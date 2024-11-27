496133e74d7ca23c8825b29178c2197a3ca7c6c1
Elena Suárez <esuaser650@g.educaand.es>
2024-11-27 17:53:21 +0100

    Tema añadido

--- app/src/main/java/com/example/appstem/navigation/NavigationHost.kt
+++ app/src/main/java/com/example/appstem/navigation/NavigationHost.kt
@@ -9,22 +9,26 @@ import androidx.navigation.compose.composable
 import com.example.appstem.screens.BiografiaHypatia
 import com.example.appstem.screens.Home
 import com.example.appstem.screens.ScrollBios
+import com.example.appstem.ui.theme.AppStemTheme
 
 @Composable
 fun NavigationHost(navController: NavHostController) {
-    NavHost(
-        navController = navController,
-        startDestination = AppScreens.Home.route,
-        modifier = Modifier.fillMaxSize()
-    ) {
-        composable(AppScreens.Home.route) {
-            Home(navController)
-        }
-        composable(AppScreens.ScrollBios.route) {
-            ScrollBios(navController)
-        }
-        composable(AppScreens.Bio.route) {
-            BiografiaHypatia(navController)
+    // Aplicar el tema a todo el NavHost
+    AppStemTheme {
+        NavHost(
+            navController = navController,
+            startDestination = AppScreens.Home.route,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            composable(AppScreens.Home.route) {
+                Home(navController)
+            }
+            composable(AppScreens.ScrollBios.route) {
+                ScrollBios(navController)
+            }
+            composable(AppScreens.Bio.route) {
+                BiografiaHypatia(navController)
+            }
         }
     }
 }
